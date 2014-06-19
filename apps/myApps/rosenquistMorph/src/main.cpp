7e02f78aea23234d81c947b9eeade15fc1df1a5c
Steve Minor <steve.e.minor@nordstrom.com>
2014-06-19 14:35:59 -0700

    final adjustments to rosenquistMorph, syncing webcam deally with windows machine post branch refresh

--- apps/myApps/rosenquistMorph/src/main.cpp
+++ apps/myApps/rosenquistMorph/src/main.cpp
@@ -7,7 +7,7 @@ int main( ){
 //    this interferes with openCV in some way, will need to drop down to #version 120 shaders
 //    ofSetCurrentRenderer(ofGLProgrammableRenderer::TYPE);
     ofAppGlutWindow window;
-	ofSetupOpenGL(&window, 1920, 1080, OF_FULLSCREEN);			// <-------- setup the GL context
+	ofSetupOpenGL(&window, 1080, 1920, OF_FULLSCREEN);			// <-------- setup the GL context
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
