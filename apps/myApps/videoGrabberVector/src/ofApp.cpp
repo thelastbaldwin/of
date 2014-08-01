066ea1044524fedbe454e0e24e6925f5a9e4dbfa
Steve Minor <stv.mnr@gmail.com>
2014-07-31 20:13:55 -0700

    got 10 cameras working

--- apps/myApps/videoGrabberVector/src/ofApp.cpp
+++ apps/myApps/videoGrabberVector/src/ofApp.cpp
@@ -2,28 +2,29 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-	
-	camWidth 		= 320;	// try to grab at this size. 
+
+	camWidth 		= 320;	// try to grab at this size.
 	camHeight 		= 240;
-	
-    //we can now get back a list of devices. 
+
+    //we can now get back a list of devices.
 	vector<ofVideoDevice> devices = vidGrabber.listDevices();
-	
+
     for(int i = 0; i < devices.size(); i++){
-		cout << devices[i].id << ": " << devices[i].deviceName; 
-        if( devices[i].bAvailable && devices[i].deviceName != "FaceTime HD Camera"){
+		cout << devices[i].id << ": " << devices[i].deviceName;
+        if( devices[i].bAvailable){
             cout << endl;
             ofVideoGrabber grabber;
             cams.push_back(grabber);
             cams[i].setDeviceID(i);
+            cams[i].setDesiredFrameRate(30);
             cams[i].initGrabber(camWidth,camHeight);
-            
+
         }else{
-            cout << " - unavailable " << endl; 
+            cout << " - unavailable " << endl;
         }
 	}
-    
-    
+
+
     std::cout << cams.size() << " cameras" << std::endl;
 	ofSetVerticalSync(true);
 }
@@ -31,11 +32,11 @@ void ofApp::setup(){
 
 //--------------------------------------------------------------
 void ofApp::update(){
-	
+
 	ofBackground(100,100,100);
-	
-	for(auto cam: cams){
-        cam.update();
+
+    for(int i = 0; i < cams.size(); ++i){
+        cams[i].update();
     }
 
 }
@@ -44,51 +45,59 @@ void ofApp::update(){
 void ofApp::draw(){
 	ofSetHexColor(0xffffff);
     int offset = 0;
-	for(auto cam: cams){
+	/*for(auto cam: cams){
         cam.draw(offset * 320, 0);
         ++offset;
+    }*/
+    for(int i = 0; i < cams.size(); ++i){
+        if(i < 5){
+            cams[i].draw((i % 5) * 320, 0);
+        }
+        else{
+            cams[i].draw((i % 5) * 320, 240);
+        }
     }
 }
 
 
 //--------------------------------------------------------------
-void ofApp::keyPressed  (int key){ 
-	
-	// in fullscreen mode, on a pc at least, the 
+void ofApp::keyPressed  (int key){
+
+	// in fullscreen mode, on a pc at least, the
 	// first time video settings the come up
 	// they come up *under* the fullscreen window
 	// use alt-tab to navigate to the settings
 	// window. we are working on a fix for this...
-	
+
 	// Video settings no longer works in 10.7
 	// You'll need to compile with the 10.6 SDK for this
     // For Xcode 4.4 and greater, see this forum post on instructions on installing the SDK
-    // http://forum.openframeworks.cc/index.php?topic=10343        
+    // http://forum.openframeworks.cc/index.php?topic=10343
 	if (key == 's' || key == 'S'){
 		vidGrabber.videoSettings();
 	}
-	
-	
+
+
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){ 
-	
+void ofApp::keyReleased(int key){
+
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y ){
-	
+
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
-	
+
 }
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-	
+
 }
 
 //--------------------------------------------------------------
@@ -107,6 +116,6 @@ void ofApp::gotMessage(ofMessage msg){
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::dragEvent(ofDragInfo dragInfo){
 
 }
