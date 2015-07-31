337d8fa1d51e676e19f9d4b1785517c27ebc5925
Steve Minor <stv.mnr@gmail.com>
2015-07-31 15:49:53 -0700

    UI changes for Dimitri and Mylan

--- apps/myApps/ghettoMatrix/bin/server/js/client.js
+++ apps/myApps/ghettoMatrix/bin/server/js/client.js
@@ -1,12 +1,42 @@
 var socket = io();
 var container = document.getElementsByClassName('container')[0];
+var modal = document.getElementById('modal-overlay');
+var timer = document.getElementsByClassName('counter')[0];
+var counterTags = document.getElementsByClassName('counter-tag');
 var img = document.getElementsByTagName('img')[0];
 var IMGPATH = 'img/';
 
 // DOM events
 container.addEventListener('click', function(e){
 	if(event.srcElement.type === "button") {
-		socket.emit('take photo', { type : e.srcElement.value});
+		if(e.srcElement.value == "traditional"){
+			var steps = 16;
+			timer.innerHTML = '';
+			modal.classList.remove('hide');
+
+			for(var i = 0 ; i < counterTags.length; i++){
+				counterTags[i].classList.add('hide');
+			}
+			var currentCounterTag = 0;
+			var countdown = setInterval(function(){
+				steps--;
+				var currentCount = timer.innerHTML = steps % 4;
+				if(currentCount === 0){
+					counterTags[currentCounterTag].classList.remove('hide');
+					socket.emit('take photo', { type : e.srcElement.value, index: currentCounterTag});
+					currentCounterTag++;
+				}
+				if(steps < 1){
+					setTimeout(function(){
+						clearInterval(countdown);
+						modal.classList.add('hide');
+					}, 200);	
+				}
+			}, 1000);
+	
+		}else{
+			socket.emit('take photo', { type : e.srcElement.value});
+		}
 	}
 	e.stopPropagation();
 }, false);
